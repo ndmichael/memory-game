100bded00ca7890cb0ab83de02efbfa531ad9410
ndmichael <ukejemicheal@gmail.com>
2023-12-12 14:18:13 +0100

    reset board after every unmatched

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ flipCard = function(e){
     // lock board until unmatched card flipped back
     // this solve the bug of card flipping.
     if(lockBoard) return;
+    if(this === firstCard) return;
 
     this.classList.add('flip')
 
@@ -25,6 +26,7 @@ flipCard = function(e){
         if(firstCard.dataset.name === secondCard.dataset.name){
             firstCard.removeEventListener('click', flipCard);
             secondCard.removeEventListener('click', flipCard);
+            resetBoard();
         }
         // else if its not a match
         // DELAY 1 seconds before flipping back
@@ -33,11 +35,16 @@ flipCard = function(e){
             setTimeout(()=>{
                 firstCard.classList.remove('flip');
                 secondCard.classList.remove('flip');
-                lockBoard = false;
+                resetBoard();
             }, 1000)  ;  
             
         }
     }
+}
+
+let resetBoard =  function(){
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard))
