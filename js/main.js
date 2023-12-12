4f1e78490887552ea71d4b544343e61f33bed7b0
ndmichael <ukejemicheal@gmail.com>
2023-12-12 20:25:48 +0100

    count logic for trials

--- js/main.js
+++ js/main.js
@@ -4,6 +4,30 @@ let hasFlippedCard = false;
 let firstCard, secondCard;
 let lockBoard = false;
 
+// shuffle card function
+function cardShuffle(){
+    cards.forEach(card =>{
+        let randPos = Math.floor(Math.random() * 12);
+        card.style.order = randPos;
+    })
+};
+
+cardShuffle();
+
+var count = 0;
+
+function counter(count=0){
+    const trials = document.querySelector('.trials');
+    console.log(trials)
+    trials.innerHTML = count;
+}
+
+function scores(){
+    let scores = 0;
+    scores++;
+
+}
+
 flipCard = function(e){
     // lock board until unmatched card flipped back
     // this solve the bug of card flipping.
@@ -19,8 +43,11 @@ flipCard = function(e){
     }
     // second flipped
     else{
-        hasFlippedCard = false;
+        // hasFlippedCard = false;
         secondCard = this;
+        count++;
+        
+        counter(count);
         
         // if its a match
         if(firstCard.dataset.name === secondCard.dataset.name){
@@ -39,6 +66,7 @@ flipCard = function(e){
             }, 1000)  ;  
             
         }
+        
     }
 }
 
@@ -46,5 +74,12 @@ let resetBoard =  function(){
     [hasFlippedCard, lockBoard] = [false, false];
     [firstCard, secondCard] = [null, null];
 }
+
+/*
+    * call function immediately
+    * generate random numbers for card positions
+    * assign to flex which auto shuffle order
+*/
+
 
 cards.forEach(card => card.addEventListener('click', flipCard))
