9eac475264add98805c6ac8b8906eecedf026446
ndmichael <ukejemicheal@gmail.com>
2023-12-13 12:36:35 +0100

    comments and conditional logic

--- js/leadership.js
+++ js/leadership.js
@@ -1,19 +1,29 @@
+/*
+    ** Logic to fetch records data from localStorage
+    ** @Return it to leaderboard.html as table
+    ** Condition if record is empty return table with string text
+*/
 function show_leadership(){
     var tables = "";
     let records = JSON.parse(localStorage.getItem('records'))
-
-    records.forEach((record, count)=> {
-        tables += "<tr>" + 
-        "<td>"+ ++count  +"</td>" +
-            "<td>"+ record.username  +"</td>" +
-            "<td>"+ record.fullname  +"</td>" +
-            "<td>"+ record.trials  +"</td>" +
-            "<td>"+ record.score  +"</td>" +
-        
-        "</tr>"
-    });
-
-    document.getElementById("table").innerHTML = '<table>' + tables + '</table>';
+    if(records !== null){
+        records.forEach((record, count)=> {
+            tables += "<tr>" + 
+            "<td>"+ ++count  +"</td>" +
+                "<td>"+ record.username  +"</td>" +
+                "<td>"+ record.fullname  +"</td>" +
+                "<td>"+ record.trials  +"</td>" +
+                "<td>"+ record.score  +"</td>" +
+            
+            "</tr>"
+        });
+    }
+    else{
+        tables += `<tr><td><h2>No record sign up / login and start playing</h2></td></tr>`;
+    }
+   
+    console.log(tables)
+    document.getElementById("table").innerHTML =  tables ;
 }
 
 show_leadership();
